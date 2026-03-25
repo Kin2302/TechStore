e711f85a8cd2db9310fe32b9483aca5cb92fdd3b
Kin2302 <kie7s5@gmail.com>
2026-03-25 16:18:39 +0700

    add update-role funtion

--- Application/DTOs/Admin/UserInfoDto.cs
+++ Application/DTOs/Admin/UserInfoDto.cs
@@ -5,6 +5,7 @@ namespace Application.DTOs.Admin
         public string UserId { get; set; } = "";
         public string Email { get; set; } = "";
         public string UserName { get; set; } = "";
+        public string Role { get; set; } = "User";
         public int TotalOrders { get; set; }
         public decimal TotalSpent { get; set; }
     }
--- Application/Interfaces/Admin/IUserService.cs
+++ Application/Interfaces/Admin/IUserService.cs
@@ -5,6 +5,9 @@ namespace Application.Interfaces.Admin
     public interface IUserService
     {
         Task<List<UserInfoDto>> SearchUsersAsync(string query);
+        Task<List<UserInfoDto>> GetAllUsersAsync();
         Task<UserInfoDto?> GetUserByIdAsync(string userId);
+        Task<bool> UpdateUserRoleAsync(string userId, string role);
     }
 }
+    
